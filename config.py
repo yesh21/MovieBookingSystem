3bb2b5af07cb2f02339bd4ac046da4baeb341220
yesh21 <yaswanth.pulavarthi@gmail.com>
2021-02-20 09:49:06 +0530

    added db config

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 # Production
 class DefaultConfig(object):
     DEBUG = False
@@ -12,13 +14,18 @@ class DefaultConfig(object):
 
 class DevelopmentConfig(DefaultConfig):
     DEBUG = True
-
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'site.db')
     SAFE_URL_HOSTS = {'127.0.0.1:5000'}
 
 
 class ProductionConfig(DefaultConfig):
     SECRET_KEY = None
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'site.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SECRET_KEY = 'e327924a6b8846030dbd537a6a33c7f0f2149593b50bc235016e022846915444'
 
 
 class TestConfig(DefaultConfig):
