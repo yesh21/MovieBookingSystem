e9d1f83b0a70302f5948727a1b2e50884971938a
aaron rosser <aaron25th@gmail.com>
2021-02-24 13:43:36 +0000

    Started adding film previews, cleaned up models.py

--- Flask_Cinema_Site/movie/views.py
+++ Flask_Cinema_Site/movie/views.py
@@ -1,6 +1,9 @@
 from Flask_Cinema_Site.movie.forms import NewMovieForm
+from Flask_Cinema_Site.models import Movie
 
 from flask import render_template, redirect, url_for, Blueprint
+
+from datetime  import date
 
 movies_blueprint = Blueprint(
     'movie', __name__,
@@ -10,7 +13,18 @@ movies_blueprint = Blueprint(
 
 @movies_blueprint.route('/', methods=['GET'])
 def view_multiple():
-    return render_template('view_multiple_movies.html')
+    m = Movie()
+    m.name = 'Black Widow'
+    m.overview = 'In Marvel Studios’ action-packed spy thriller “Black Widow,” Natasha Romanoff aka Black Widow confronts the darker parts of her ledger when a dangerous conspiracy with ties to her past arises. Pursued by a force that will stop at nothing to bring her down, Natasha must deal with her history as a spy and the broken relationships left in her wake long before she became an Avenger.'
+    m.released = date(2021, 5, 7)
+    m.cover_art_name = 'black_widow.jpg'
+    m.directors = 'Cate Shortland'
+    m.cast = 'Rachel Weisz, David Harbour, O-T Fagbenle, Ray Winstone, Florence Pugh, Scarlett Johansson'
+
+    movies = [
+        'm'
+    ]
+    return render_template('view_multiple_movies.html', title='Browse Movies', movies=movies)
 
 
 @movies_blueprint.route('/<int:film_id>', methods=['GET'])
