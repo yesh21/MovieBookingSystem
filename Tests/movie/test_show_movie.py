510c706a1e97c8d3b99a2baf60c36e6be39dc9ec
aaron rosser <sc19ar@leeds.ac.uk>
2021-03-17 00:58:24 +0000

    Fixed authorization tests

--- Tests/movie/test_show_movie.py
+++ Tests/movie/test_show_movie.py
@@ -8,11 +8,11 @@ from flask_api import status
 
 class ShowMovieTestCase(MovieBaseTestCase):
     def send_show_request(self, hidden):
-        self.mA.hidden = hidden
+        self.movie_A.hidden = hidden
         db.session.commit()
 
         params = dict(
-            id=self.mA.id,
+            id=self.movie_A.id,
             csrf_token=self.csrf_token
         )
         return self.client.post(url_for('movie.show'), data=params, follow_redirects=True)
@@ -23,16 +23,18 @@ class ShowMovieTestCase(MovieBaseTestCase):
             self.login_managerA()
             res = self.send_show_request(True)
 
-            self.assert_json_response(res, f'Movie with id \'{self.mA.id}\' successfully unhidden', status.HTTP_200_OK)
-            self.assertEqual(self.mA.hidden, False)
+            self.assert_json_response(res, f'Movie with id \'{self.movie_A.id}\' successfully unhidden',
+                                      status.HTTP_200_OK)
+            self.assertEqual(self.movie_A.hidden, False)
 
     def test_show_visible_movie_manager(self):
         with self.client:
             self.login_managerA()
             res = self.send_show_request(False)
 
-            self.assert_json_response(res, f'Movie with id \'{self.mA.id}\' successfully unhidden', status.HTTP_200_OK)
-            self.assertEqual(self.mA.hidden, False)
+            self.assert_json_response(res, f'Movie with id \'{self.movie_A.id}\' successfully unhidden',
+                                      status.HTTP_200_OK)
+            self.assertEqual(self.movie_A.hidden, False)
 
     # Fail
     def test_show_movie_non_existent(self):
@@ -50,7 +52,7 @@ class ShowMovieTestCase(MovieBaseTestCase):
     def test_show_hidden_movie_no_id(self):
         with self.client:
             self.login_managerA()
-            self.mA.hidden = True
+            self.movie_A.hidden = True
             db.session.commit()
 
             params = dict(
@@ -59,32 +61,33 @@ class ShowMovieTestCase(MovieBaseTestCase):
             res = self.client.post(url_for('movie.show'), data=params, follow_redirects=True)
 
             self.assert_json_response(res, 'Error show movie failed', status.HTTP_400_BAD_REQUEST)
-            self.assertEqual(self.mA.hidden, True)
+            self.assertEqual(self.movie_A.hidden, True)
 
     def test_show_hidden_movie_no_csrf(self):
         with self.client:
             self.login_managerA()
-            self.mA.hidden = True
+            self.movie_A.hidden = True
             db.session.commit()
 
             params = dict(
-                id=self.mA.id
+                id=self.movie_A.id
             )
             res = self.client.post(url_for('movie.show'), data=params, follow_redirects=True)
 
             self.assert_json_response(res, 'Error show movie failed', status.HTTP_400_BAD_REQUEST)
-            self.assertEqual(self.mA.hidden, True)
+            self.assertEqual(self.movie_A.hidden, True)
 
     def test_show_hidden_movie_customer(self):
         with self.client:
-            self.login_managerA()
+            self.login_customerA()
             res = self.send_show_request(True)
 
-            # TODO
+            self.assert401(res)
+            self.assertEqual(self.movie_A.hidden, True)
 
     def test_show_hidden_movie_unauthenticated(self):
         with self.client:
-            self.login_managerA()
             res = self.send_show_request(True)
 
-            # TODO
+            self.assert401(res)
+            self.assertEqual(self.movie_A.hidden, True)
