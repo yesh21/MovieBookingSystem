10ebc4cb933c915a34daa48907fe0d1a06fa486b
Matt Poole <sc19map@leeds.ac.uk>
2021-03-25 00:53:08 +0000

    home page (linking not finished so still looks bad)

--- Flask_Cinema_Site/home/views.py
+++ Flask_Cinema_Site/home/views.py
@@ -1,11 +1,17 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, url_for, render_template
+from Flask_Cinema_Site import app
 
 home_blueprint = Blueprint(
     'home', __name__,
-    template_folder='templates'
+    template_folder='templates',
+    static_folder='static'
 )
 
+@app.route('/', methods=['GET'])
+def boot():
+    return redirect(url_for('home.home'))
 
 @home_blueprint.route('/', methods=['GET'])
 def home():
-    return redirect(url_for('movie.view_multiple'))
+    return render_template('home_base.html')
+    #return redirect(url_for('movie.view_multiple'))
