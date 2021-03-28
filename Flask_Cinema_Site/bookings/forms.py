b4ef58d6facc57f48f85f767bb8eb4a2985cb713
aaron rosser <aaron25th@gmail.com>
2021-03-28 13:28:05 +0100

    Improved ticket layout, fixed card payment form

--- Flask_Cinema_Site/bookings/forms.py
+++ Flask_Cinema_Site/bookings/forms.py
@@ -1,9 +1,10 @@
-from Flask_Cinema_Site.models import Seat, TicketType
+from Flask_Cinema_Site.models import Seat, SavedCard
 
+from flask_login import current_user
 from flask_wtf import FlaskForm
 
-from wtforms import StringField, PasswordField, SubmitField, HiddenField
-from wtforms.fields.html5 import EmailField
+from wtforms import StringField, PasswordField, SubmitField, HiddenField, BooleanField
+from wtforms.fields.html5 import EmailField, DateField
 from wtforms.validators import DataRequired, Length, Email, ValidationError
 
 import json
@@ -26,6 +27,20 @@ class CashPaymentForm(BasePaymentForm):
 
 
 class CardPaymentForm(BasePaymentForm):
-    card = StringField('Card', validators=[DataRequired(), Length(min=2, max=20)])
+    card = StringField('Card', validators=[DataRequired(), Length(min=16, max=19)])
     cvc = PasswordField('CVC', validators=[DataRequired()])
+    expiry = DateField('Expiry date')
+    remember = BooleanField('Remember card')
     submit = SubmitField('Pay')
+
+    def validate_card(self, card_number):
+        if not self.card.data.startswith('****'):
+            return
+
+        last_4_digits = self.card.data[-4:]
+        saved_card = SavedCard.query\
+            .filter(SavedCard.user_id == current_user.id)\
+            .filter(SavedCard.number.like(f'%{last_4_digits}'))\
+            .first()
+        if not saved_card:
+            raise ValidationError(f'Card ending \'{last_4_digits}\' not found')
--- Flask_Cinema_Site/bookings/helper_functions.py
+++ Flask_Cinema_Site/bookings/helper_functions.py
@@ -1,99 +1,155 @@
-from Flask_Cinema_Site.models import Transaction, User, Viewing
+from Flask_Cinema_Site import app
+from Flask_Cinema_Site.models import Transaction, Viewing
+from Flask_Cinema_Site.helper_functions import send_email_with_attachment
 
-from reportlab.platypus import SimpleDocTemplate, Table, Paragraph, TableStyle
+from flask import render_template, current_app
+
 from reportlab.lib import colors
-from reportlab.lib.pagesizes import A4
-from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
+from reportlab.graphics.barcode.qr import QrCodeWidget
+from reportlab.graphics.shapes import Drawing
+from reportlab.graphics import renderPDF
+from reportlab.platypus import Table, TableStyle
 
 import os
-from datetime import datetime
-
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
+import jwt
 
 
-def create_pdf(transaction, email_address):
-    user = transaction.user
+def generate_receipt_pdf(transaction, email_address):
     viewing = transaction.viewings[0]
-    movie = viewing.movie
 
-    count = 0
-    data_array = []
+    path = transaction.get_receipt_path()
+    c = canvas.Canvas(path)
+    width, height = letter
+    margin = 50
 
-    data_array.append(["Email address", "Date", "Movie", "Seats"])
+    logo_path = os.path.join(current_app.root_path, "bookings", "static", 'img', 'moviebox.png')
+    c.drawImage(logo_path, width / 2 - 240 / 2, 750, width=240, height=41, mask='auto')
 
-    for seat in transaction.seats:
-        if count == 0:
-            data_array.append([str(user.email), str(transaction.datetime), str(movie.name),
-                              str(seat.seat_number)])
-        else:
-            data_array.append(["", "", "", str(seat.seat_number)])
-        count = count + 1
+    c.setFont('Helvetica', 30)
+    c.drawString(margin, 720, 'Tickets')
 
-    vat = viewing.price * 0.2
+    draw_movie_viewing_section(c, margin, 700, viewing, email_address)
+    draw_transaction_section(c, 370, 700, transaction)
 
-    data_array.append(["Sub total", "", "", str(viewing.price)])
-    data_array.append(["VAT", "", "", str(vat)])
-    data_array.append(["Total", "", "", str(viewing.price + vat)])
+    qr_data = jwt.encode(
+        payload={'transaction_id': transaction.id, 'viewing_id': viewing.id},
+        key=app.config['SECRET_KEY'],
+        algorithm='HS256'
+    )
+    draw_qr_code(c, margin, 410, qr_data)
+    # 180 = qr code height, 22 = qr code border width
+    draw_seats_table(c, width - margin, 410 + 180 - 22, transaction)
 
-    dir = os.getcwd()
-    path = os.path.join(dir, "Flask_Cinema_Site", "bookings", "receipts", "receipt.pdf")
+    c.save()
 
-    pdf = SimpleDocTemplate(path, pagesize=A4)
-    styles = getSampleStyleSheet()
-    title_style = styles["Heading1"]
-    title_style.alignment = 1
-    title = Paragraph("MovieBox receipt", title_style)
+    send_email_with_attachment(
+        subject=f'[MovieBox] Your tickets for {viewing.movie.name}',
+        sender=app.config['MAIL_USERNAME'],
+        recipients=[email_address],
+        text_body=render_template('email/receipt.txt', viewing=viewing),
+        html_body=render_template('email/receipt.txt', viewing=viewing),
+        file_path=path,
+        content_type='application/pdf',
+        attachment_name='tickets.pdf'
+    )
 
-    style = TableStyle(
+
+def draw_movie_viewing_section(c, x, y, viewing: Viewing, email_address):
+    second_margin = x + 100
+    c.setFont('Helvetica', 15)
+
+    c.drawString(x, y, 'Movie:')
+    c.drawString(second_margin, y, viewing.movie.name)
+
+    # Viewing section
+    viewing_offset = 10
+    c.drawString(x, y - 20, 'Viewing:')
+
+    c.drawString(x + viewing_offset, y - 20 * 2, 'Number:')
+    c.drawString(second_margin, y - 20 * 2, str(viewing.id))
+
+    c.drawString(x + viewing_offset, y - 20 * 3, 'Date:')
+    c.drawString(second_margin, y - 20 * 3, viewing.time.strftime('%d/%m/%y'))
+
+    c.drawString(x + viewing_offset, y - 20 * 4, 'Time:')
+    c.drawString(second_margin, y - 20 * 4, viewing.time.strftime('%H:%M'))
+
+    # Email
+    c.drawString(x, y - 20 * 5, 'Email:')
+    c.drawString(second_margin, y - 20 * 5, email_address)
+
+
+def draw_transaction_section(c, x, y, transaction: Transaction):
+    second_margin = x + 100
+    c.setFont('Helvetica', 15)
+
+    transaction_offset = 10
+    c.drawString(x, y, 'Transaction details:')
+
+    c.drawString(x + transaction_offset, y - 20, 'Number:')
+    c.drawString(second_margin, y - 20, str(transaction.id))
+
+    c.drawString(x + transaction_offset, y - 20 * 2, 'Date:')
+    c.drawString(second_margin, y - 20 * 2, transaction.datetime.strftime('%d/%m/%y'))
+
+    c.drawString(x + transaction_offset, y - 20 * 3, 'Time:')
+    c.drawString(second_margin, y - 20 * 3, transaction.datetime.strftime('%H:%M'))
+
+
+def draw_seats_table(c, x, y, transaction: Transaction):
+    table_style = TableStyle(
         [
-            ("BOX", (0, 0), (-1, -1), 1, colors.black),
-            ("GRID", (0, 0), (-1, -1), 1, colors.black),
-            ("BACKGROUND", (0, 0), (-1, -1), colors.gray),
-            ("TEXTCOLOR", (0, 0), (-1, 0), colors.whitesmoke),
-            ("ALIGN", (0, 0), (-1, -1), "CENTER"),
-            ("BACKGROUND", (0, 1), (-1, -1), colors.beige),
+            ('BOX', (0, 0), (-1, -1), 1, colors.black),
+            ('GRID', (0, 0), (-1, -1), 1, colors.black),
+            ('BACKGROUND', (0, 0), (-1, -1), colors.gray),
+            ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+            ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+            ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
         ]
     )
 
-    table = Table(data_array, style=style)
-    pdf.build([title, table])
-    send_pdf(path, email_address)
+    table_data = [
+        ['Seat number', 'Ticket type', 'Price']
+    ]
+    for seat in transaction.seats:
+        table_data.append([
+            seat.seat_number,
+            seat.ticket_type.name,
+            '£{:.2f}'.format(seat.ticket_type.price)
+        ])
+    table_data.append([[]])
+    table_data.append([
+        'VAT',
+        '',
+        '£{:.2f}'.format(transaction.get_cost() * 0.2)
+    ])
+    table_data.append([
+        'Total',
+        '',
+        '£{:.2f}'.format(transaction.get_cost())
+    ])
+
+    f = Table(table_data, style=table_style)
+    f.wrapOn(c, 400, 100)
+    table_width, table_height = f.wrap(0, 0)
+
+    # Magic numbers 17 just works lol, 715 is the height of the top of 'Movie:'
+    f.drawOn(c, x - table_width - 17, y - table_height)
 
 
-def send_pdf(path, email):
-    from_addr = "alan.ashford.786123@gmail.com"
-    toaddr = str(email)
-    today_date = datetime.now()
+def draw_qr_code(c, x, y, qr_data):
+    # QR code
+    qr_code = QrCodeWidget(qr_data)
+    bounds = qr_code.getBounds()
 
-    msg = MIMEMultipart()
-    msg['From'] = from_addr
-    msg['To'] = toaddr
-    msg['Subject'] = "MovieBox receipt"
+    qr_width = bounds[2] - bounds[0]
+    qr_height = bounds[3] - bounds[1]
 
-    body = "Please see your MovieBox receipt attached for the "\
-        + "transaction which occured on: " + str(today_date)\
-        + "."
-    msg.attach(MIMEText(body, 'plain'))
+    # 180 = new height and width
+    d = Drawing(180, 180, transform=[180. / qr_width, 0, 0, 180. / qr_height, 0, 0])
+    d.add(qr_code)
 
-    filename = "receipt.pdf"
-    attachment = open(path, "rb")
-
-    p = MIMEBase('application', 'octet-stream')
-    p.set_payload((attachment).read())
-    encoders.encode_base64(p)
-    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-    msg.attach(p)
-
-    s = smtplib.SMTP('smtp.gmail.com', 587)
-    s.starttls()
-    s.login(from_addr, "Laughing522")
-    text = msg.as_string()
-    s.sendmail(from_addr, toaddr, text)
-    s.quit()
-
-    attachment.close()
-    os.remove(path)
+    # 22 = qr code border width
+    renderPDF.draw(d, c, x - 22, y)
